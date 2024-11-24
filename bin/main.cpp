e44610c8d5033a307e255dac6f6ee35e63f49929
Дмитрий <93881631+mityaiii@users.noreply.github.com>
2024-11-24 13:12:41 +0300

    Antlr4 tests (#1)
    * save
    
    * feat: added tests fix: cmake

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,16 +1,19 @@
 #include "antlr4-runtime.h"
 #include "GrammarLexer.h"
 #include "GrammarParser.h"
-#include "YalLLVM.h" 
+#include "YalLLVM.h"
 
 #include <filesystem>
 #include <iostream>
 #include <fstream>
 #include <string>
 
-int main() {
-    std::cout << "Current working directory: " 
-              << std::filesystem::current_path() << std::endl;
+int main()
+{
+  std::cout << "Current working directory: "
+            << std::filesystem::current_path() << std::endl;
+
+  std::cout << "./src/example.txt";
 
   std::string program = R"(
   
@@ -19,10 +22,11 @@ int main() {
   )";
 
   YalLLVM vm;
-  vm.exec(program);
+  // vm.exec(program);
 
-  std::ifstream stream("../src/example.txt");
-  if (!stream) {
+  std::ifstream stream("./src/example.txt");
+  if (!stream)
+  {
     std::cerr << "Cannot open input file\n";
     return 1;
   }
@@ -31,8 +35,8 @@ int main() {
   GrammarLexer lexer(&input);
   antlr4::CommonTokenStream tokens(&lexer);
   GrammarParser parser(&tokens);
-  GrammarParser::ProgContext* tree = parser.prog();
+  GrammarParser::ProgContext *tree = parser.prog();
 
-  std::cout << "Parsed: " << tree->toStringTree(&parser) << std::endl;
+  // std::cout << "Parsed: " << tree->toStringTree(&parser) << std::endl;
   return 0;
 }
